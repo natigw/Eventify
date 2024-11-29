913246506e958a4803240f554a48b6ece0ade959
Tural227 <turalbadalzade@gmail.com>
2024-11-29 13:29:04 +0400

    Places Progress bar added

--- app/src/main/java/com/example/eventify/presentation/viewmodels/EventViewModel.kt
+++ app/src/main/java/com/example/eventify/presentation/viewmodels/EventViewModel.kt
@@ -6,6 +6,7 @@ import com.example.eventify.domain.model.EventItem
 import com.example.eventify.domain.repository.EventRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -14,6 +15,7 @@ class EventViewModel @Inject constructor(
     private val eventRepository: EventRepository
 ) : ViewModel() {
 
+    val isLoading = MutableStateFlow(true)
     val events = MutableStateFlow<List<EventItem>>(emptyList())
 
     init {
@@ -24,6 +26,7 @@ class EventViewModel @Inject constructor(
         viewModelScope.launch {
             val response = eventRepository.getEvents()
             events.emit(response)
+            isLoading.update { false }
         }
     }
 }
