5368b91d8afcb8f63e2e96daf8b6140a6cdaf044
Tural227 <turalbadalzade@gmail.com>
2024-12-10 01:38:31 +0400

    login set

--- app/src/main/java/com/example/eventify/data/remote/interceptor/AuthInterceptor.kt
+++ app/src/main/java/com/example/eventify/data/remote/interceptor/AuthInterceptor.kt
@@ -21,9 +21,8 @@ class AuthInterceptor @Inject constructor(
         val tokenType = "Bearer"
 
 
-        val temp = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJibHVlc3RlZWwyIiwiaWQiOjM0LCJleHAiOjE3MzM3ODA4OTF9.AMEQM0pvozwZvdicBXJfRxnGLP-kWSsN7cJkNAWvc9Y"
         request = request.newBuilder()
-            .addHeader("Authorization","$tokenType $temp")
+            .addHeader("Authorization","$tokenType $accessToken")
             .build()
 
         val response = chain.proceed(request)
