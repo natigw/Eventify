54f45f83ee2f41ad5733d6052efff2f1994375e1
Tural227 <turalbadalzade@gmail.com>
2024-12-10 01:45:03 +0400

    login set

--- app/src/main/java/com/example/eventify/data/remote/interceptor/AuthInterceptor.kt
+++ app/src/main/java/com/example/eventify/data/remote/interceptor/AuthInterceptor.kt
@@ -9,12 +9,12 @@ import javax.inject.Inject
 import javax.inject.Named
 
 
-//lazimsiz gorsenir cunki refresh token authorization istemir !!!!!!!!!!!!!!!!!!!!!!!
 class AuthInterceptor @Inject constructor(
     @Named("UserTokens") private val sharedPrefUserTokens: SharedPreferences,
 ) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         var request = chain.request()
+
 
 
         val accessToken = sharedPrefUserTokens.getString("access_token",null)
@@ -26,7 +26,6 @@ class AuthInterceptor @Inject constructor(
             .build()
 
         val response = chain.proceed(request)
-
         if(response.code() == 401){
             AppUtils.authChannel.trySend(RequestChannel.ON_401_ERROR)
         }
