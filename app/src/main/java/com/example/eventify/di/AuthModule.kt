3834dc45df1c87c690257a477fb4c4cb20cba60b
Tural227 <turalbadalzade@gmail.com>
2024-12-14 23:10:17 +0400

    login set

--- app/src/main/java/com/example/eventify/di/AuthModule.kt
+++ app/src/main/java/com/example/eventify/di/AuthModule.kt
@@ -1,10 +1,8 @@
 package com.example.eventify.di
 
-import android.content.Context
 import android.content.SharedPreferences
-import android.media.session.MediaSession.Token
-import com.example.eventify.common.utils.AppUtils
 import com.example.eventify.data.remote.interceptor.AuthInterceptor
+import com.example.eventify.data.remote.interceptor.RetryInterceptor
 import com.example.eventify.data.remote.interceptor.TokenManager
 import com.example.eventify.domain.repository.AuthRepository
 import dagger.Module
@@ -21,10 +19,14 @@ import javax.inject.Singleton
 class AuthModule {
     @Singleton
     @Provides
-    fun provideOkHttpClient(authInterceptor: AuthInterceptor) : OkHttpClient{
+    fun provideOkHttpClient(
+        authInterceptor: AuthInterceptor,
+        retryInterceptor: RetryInterceptor
+    ) : OkHttpClient{
         return OkHttpClient
             .Builder()
             .addInterceptor(authInterceptor)
+//            .addInterceptor(retryInterceptor)
             .build()
     }
 
